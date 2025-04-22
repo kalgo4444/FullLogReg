be32ac7bc57b9a1930a8948ade5d48b15a395ead
kalgo4444 <abuxyziabd@gmail.com>
2025-04-23 01:31:07 +0500

    add js

---
+++ assets/js/app.js
@@ -0,0 +1,91 @@
+// for sikl
+{
+    // for 1
+    // let k = 7;
+    // let n = 10;
+    // if (n > 0){
+    //     for (let i = 0; i < n; i++){
+    //         console.log(k);
+    //     }
+    // }
+}
+{
+    // for 2
+    // let a = 3, b = 8;
+    // for (a; a <= b; a++){
+    //     console.log(a);
+    // }
+}
+{
+    // for 3
+    // let a = 3, b = 8;
+    // for (a++; a < b; a++){
+    //     console.log(a);
+    // }
+}
+{
+    // for 4
+    // let kgPrice = 35_500;
+    // let sum = 0;
+    // for (let i = 1; i <= 10; i++){
+    //     sum += kgPrice
+    //     console.log(`${i} kg ${sum}`);
+    // }
+}
+{
+    // for 5
+    // let kgPrice = 3_500, sum = 0;
+    // for (let i = 1; i <= 10; i++){
+    //     let r = i / 10;
+    //     sum = kgPrice * r;
+    //     console.log(`${i} kg ${sum}`);
+    // }
+}
+{
+    // for 6
+    // let kgPrice = 3_550;
+    // for (let i = 0.2; i <= 2.0; i += 0.2){
+    //     let r = kgPrice  * i
+    //     console.log(`${i.toFixed(1)} kg ${r.toFixed(2)} som`);
+    // }
+}
+{
+    // for 7
+    // let a = 1, b = 3; sum = 0;
+    // for (a; a <= b; a++){
+    //     sum += a        
+    // }
+    // console.log(sum);
+}
+{
+    // for 8
+    // let a = 1, b = 3; sum = 0;
+    // for (a; a <= b; a++){
+    //     sum = a * a        
+    // }
+    // console.log(sum);
+}
+{
+    // for 9
+    // let a = 1, b = 3; sum = 0;
+    // for (a; a <= b; a++){
+    //     sum = (a ** 2) + (a ** 2) 
+    // }
+    // console.log(sum);
+}
+{
+    // for 10 
+    // let n = 10, sum = 0;
+    // for (let i = 1; i <= n; i++){
+    //     sum += 1 / i;        
+    // }
+    // console.log(sum);
+}
+{
+    // for 11
+    // let n = 7, sum = 0;
+    // for (let i = 1; i <= n; i++){
+    //     sum += ((n + i) ** 2);
+    // }
+    // console.log(sum);
+}
